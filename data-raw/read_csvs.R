a96eb8005b97ade9feacaabc6de2e940a4cd581b
DaveParr <davidrgparr@gmail.com>
2018-11-04 19:50:40 +0000

    start package with simple data read
    data sourced from veekun via pokeapi

---
+++ data-raw/read_csvs.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+# List files in directory
+file_list <- list.files("./data-raw/csv", "*.csv")
+
+# Read as list of tibbles
+file_list %>%
+  map( ~ read_csv(paste0("./data-raw/csv/", .))) %>%
+  set_names(., str_replace(file_list, ".csv", "")) -> pokedex
+
+usethis::use_data(pokedex)
