96ad753d1f8f4ccbd6982f75a46a480ef2251854
Dave Parr <DavidRGParr@gmail.com>
2018-11-07 22:12:32 +0000

    rename

--- data-raw/process.R
+++ data-raw/process.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 
 # List files in directory
-file_list <- list.files("./data-raw/csv", "*.csv")
+file_list <- list.files("./data-raw/", "*.csv")
 
 # Read as list of tibbles
 file_list %>%
-  map( ~ read_csv(paste0("./data-raw/csv/", .))) %>%
+  map( ~ read_csv(paste0("./data-raw/", .))) %>%
   set_names(., str_replace(file_list, ".csv", "")) -> pokedex
 
-usethis::use_data(pokedex)
+usethis::use_data(pokedex, overwrite = TRUE)
+
