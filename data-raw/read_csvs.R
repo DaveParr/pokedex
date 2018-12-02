458407855b085b11633d46f339ffee2d8c394119
Dave Parr <dave@itsalocke.com>
2018-12-02 18:15:52 +0000

    include download in processing script

--- data-raw/read_csvs.R
+++ data-raw/read_csvs.R
@@ -1,4 +1,14 @@
 library(tidyverse)
+library(git2r)
+
+# Clone down veekun most recent master branch
+git2r::clone('https://github.com/veekun/pokedex', './data-raw/veekun')
+
+# Copy selected files to data-raw
+file.copy(from = './data-raw/veekun/pokedex/data/csv', to = './data-raw/', recursive = TRUE)
+
+# Delete veekun
+unlink('./data-raw/veekun', recursive = TRUE, force = TRUE)
 
 # List files in directory
 file_list <- list.files("./data-raw/csv", "*.csv")
